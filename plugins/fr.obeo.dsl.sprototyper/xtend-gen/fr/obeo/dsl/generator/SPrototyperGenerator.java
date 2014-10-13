ffa5af86289a23ac82a39ea45666d9d7ffe952dd
Goulwen Le Fur <goulwen.lefur@obeo.fr>
2014-10-13 17:16:39 +0200

    ADDED: target platform

--- plugins/fr.obeo.dsl.sprototyper/xtend-gen/fr/obeo/dsl/generator/SPrototyperGenerator.java
+++ plugins/fr.obeo.dsl.sprototyper/xtend-gen/fr/obeo/dsl/generator/SPrototyperGenerator.java
@@ -3,12 +3,8 @@
  */
 package fr.obeo.dsl.generator;
 
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.xtext.generator.IFileSystemAccess;
-import org.eclipse.xtext.generator.IGenerator;
-
 @SuppressWarnings("all")
-public class SPrototyperGenerator implements IGenerator {
-  public void doGenerate(final Resource resource, final IFileSystemAccess fsa) {
+public class SPrototyperGenerator /* implements IGenerator  */{
+  public void doGenerate(final /* Resource */Object resource, final /* IFileSystemAccess */Object fsa) {
   }
 }
